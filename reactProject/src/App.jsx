a4b639abf03faf3c62f1a57cdd11265b2f6ae9d1
Abhi7552 <123100483+Abhi7552@users.noreply.github.com>
2025-07-01 20:29:16 +0530

    day-01

---
+++ reactProject/src/App.jsx
@@ -0,0 +1,22 @@
+
+
+function App() {
+  
+  return (
+    <>
+      <h1>Day 01 - Learning React with Vite bundler | with Hitesh sir</h1>
+
+      <h2>Welcome to React</h2>
+      <p>React is a JavaScript library for building user interfaces.</p>
+      <p>It allows developers to create large web applications that can change data, without reloading the page.</p>
+      <p>React is declarative, component-based, and efficient.</p>
+      <p>It was developed by Facebook and is maintained by Facebook and a community of individual developers and companies.</p>
+      <p>React can be used as a base in the development of single-page or mobile applications.</p>
+      
+      
+    </>
+
+  )
+}
+
+export default App;
